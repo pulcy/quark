ef9879428a51917040594c2e01776b64bab53630
Ewout Prangsma <ewout@prangsma.net>
2016-06-06 09:13:44 +0200

    Updated gluon

--- defaults.go
+++ defaults.go
@@ -25,7 +25,7 @@ import (
 
 const (
 	defaultInstanceCount       = 3
-	defaultGluonImage          = "pulcy/gluon:0.17.2"
+	defaultGluonImage          = "pulcy/gluon:0.20.0"
 	defaultRebootStrategy      = "etcd-lock"
 	defaultMinOSVersion        = "835.13.0"
 	defaultGithubTokenPathTmpl = "~/.pulcy/github-token"
